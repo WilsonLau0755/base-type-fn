e7069cd31b593b624dbbec1f196753f0e164ffd8
WilsonGor <wilsonlauzf@gmail.com>
2022-11-15 21:26:11 +0800

    更改typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: './src/app.ts',
@@ -7,27 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
   },
-  devtool: "source-map",
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json", ".html"]
+    extensions: [".ts", ".js"]
   },
   module: {
     rules: [
-        // '.ts' or '.tsx' 后缀的文件将被 loadr 'awesome-typescript-loader' 处理。
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      },
-
-      // 所有输出的 '.js' 有 sourcemaps 的文件将被 'source-map-loader' 预处理。
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      }
+      { test: /\.ts$/, loader: 'ts-loader' }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({})
   ]
 }
